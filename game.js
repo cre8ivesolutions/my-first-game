203288672626afbc099616b34b80579ea917aa78
cre8ivesolutions <tammerz78@gmail.com>
2022-12-23 09:05:00 -0600

    first commit

---
+++ game.js
@@ -0,0 +1,7 @@
+// const questionIterator = require('./iteration')
+const { questions, qanda, questionIterator, totalNumberofQuestions } = require('./questions')
+
+let score = 0;
+let totalScore = totalNumberofQuestions;
+
+console.log(totalNumberofQuestions);
---
+++ iteration.js
@@ -0,0 +1,14 @@
+// const qanda = require('./questions')
+// const numberOfQuestions = qanda.length;
+
+// function questionIterator(questionArray){
+//     for (i=0; i<qanda.length; i++){setQuestion = questionArray[i]
+//     }
+//     console.log(`questionIterator() returns this = ${i}`)
+// }
+
+// questionIterator(qanda);                                                        
+
+// export default iteratorButton(){
+
+// }
