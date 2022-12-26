aa46da746ca3ef542da6fbefa486e00fb384236a
cre8ivesolutions <tammerz78@gmail.com>
2022-12-26 15:45:50 -0600

    questions and answers are populating

--- main.js
+++ main.js
@@ -1,32 +1,13 @@
-// const { totalNumberofQuestions } = require('./src/questions')
-
-// let score = 0;
-// let totalScore = totalNumberofQuestions;
-// console.log(totalNumberofQuestions)
 let startButton = document.getElementById("startButton");
+let startScreen = document.getElementById("startScreen");
+let quizConstainer = document.getElementById("quizContainer");
 
 
-function showId(id){
-    let element = document.getElementById(id);
-    console.log(element)
-    if (element.classList = hide){
-        element.classList.remove("hide")
-    } else {
-        element.classList.add("show")
-    }
-    console.log (`showId is working = ${id}`)
+
+function startGame() {
+  quizContainer.classList.remove("hide");
+  startScreen.classList.add("hide");
+  questionIterator(qanda);
+//   hideElement();
+//   addQuestionText();
 }
-showId(startButton)
-
-// function hide(){
-//     let element = document.qeurySelector();
-//     let id = document.getElementById();
-//     if (item.classList = show){
-//         element.classList.remove("show")
-//     } else {
-//         element.classList.add("show")
-//     }
-//     console.log (item)
-// }
-
-console.log(totalNumberofQuestions);
--- questions.js
+++ questions.js
@@ -30,32 +30,53 @@ const qanda = [
     ],
   },
 ];
-
+//Add the total number of questions to the correct div
 let totalNumQ = document.getElementById("totalScore");
 let totalNumberofQuestions = qanda.length;
 totalNumQ.innerHTML = totalNumberofQuestions;
 
+// function setQuestion(currentQuestionArray){
+// }
+startButton.addEventListener("click", startGame);
 
+async function questionIterator() {
 
-function setQuestion(currentQuestionArray){
-    }
-
-function questionIterator() {
   // this grabs the qanda questionNumber
   for (let i = 0; i < qanda.length; i++) {
     let currentQuestionArray = qanda[i];
-    let thisQuestionArray = currentQuestionArray.question;
-    console.log(`${thisQuestionArray}'s answers: `);
+    let currentQuestion = currentQuestionArray.question;
+    let questionElement = document.createElement("h3");
+    let questionText = currentQuestion;
+    document
+      .getElementById("questionContainer")
+      .appendChild(questionElement);
+    questionElement.innerHTML = questionText;
+    // console.log(`${currentQuestion}'s answers: `);
 
     for (const currrentAnswersArray of currentQuestionArray.answers) {
       let oneAnswer = currrentAnswersArray.answer;
-      console.log(`oneAnswer = ${oneAnswer}`); //this is the variable for one of the answers from the current questions
+      let answerElement = document.createElement("button");
+      answerElement.classList.add("answer");
+      let answerText = oneAnswer;
+      // console.log(`oneAnswer = ${oneAnswer}`);
+
+
+      document.getElementById("answers").appendChild(answerElement);
+      answerElement.innerHTML = answerText;
+      // console.log(${currrentAnswersArray})
+      // console.log(`oneAnswer = ${oneAnswer}`); //this is the variable for one of the answers from the current questions
+
+      // questionIterator(qanda); //use this to invoke the iteration of questions
+
+      function addQandAText() {
+       
+
+     
+      }
+      addQandAText();
     }
   }
 }
-
-questionIterator(qanda); //use this to invoke the iteration of questions
-
 
 // function setAnswers(){
 //   let tID = 1;
@@ -64,31 +85,26 @@ questionIterator(qanda); //use this to invoke the iteration of questions
 //   let answerText = currentQ.answers;
 //   // currentQ.answers.forEach(){
 //   //     return answerArray
-//   //     } 
+//   //     }
 //   answerElement.innerHTML = answerText;
 //   document.getElementById("answerContainer").appendChild(answerElement)
 
 // }
 
-
-
-
-
-
 // const startButton = document.getElementById("startButton")
-// startButton.addEventListener("click", 
+// startButton.addEventListener("click",
 //   questionIterator(qanda)
-// ) 
+// )
 
-   // function setQuestion(thisQuestionNumber) {
-    //   if ((thisQuestionNumber = 0)) {
-    //     console.log(`thisQuestionNumber is equal to 0`);
-    //   } else if ((thisQuestionNumber = i)) {
-    //     console.log(
-    //       `thisQuestionNumber is a value for the array equal to qanda[${thisQuestionNumber}]`
-    //     );
-    //   } else {
-    //     console.log("setQuestion() error");
-    //   }
-    //   setQuestion(thisQuestionNumber); //put the iteration in here to grab the array
-    // }
+// function setQuestion(thisQuestionNumber) {
+//   if ((thisQuestionNumber = 0)) {
+//     console.log(`thisQuestionNumber is equal to 0`);
+//   } else if ((thisQuestionNumber = i)) {
+//     console.log(
+//       `thisQuestionNumber is a value for the array equal to qanda[${thisQuestionNumber}]`
+//     );
+//   } else {
+//     console.log("setQuestion() error");
+//   }
+//   setQuestion(thisQuestionNumber); //put the iteration in here to grab the array
+// }
