3baca10b2a47f385b27e9cea60f85082d321b6e5
cre8ivesolutions <tammerz78@gmail.com>
2022-12-26 20:11:40 -0600

    more css done, added some questions, not iterating

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 let startButton = document.getElementById("startButton");
 let startScreen = document.getElementById("startScreen");
 let quizConstainer = document.getElementById("quizContainer");
-let submitButton = document.getElementById("submitButton");
+// let submitButton = document.getElementById("submitButton");
 let nextButton = document.getElementById("nextButton");
 let backButton = document.getElementById("backButton");
+let answerButton = document.getElementsByClassName("answer");
 
 
 function startGame() {
--- questions.js
+++ questions.js
@@ -1,35 +1,112 @@
 const qanda = [
   {
     id: 0,
-    questionNumber: 1,
-    question: "This is the first question?",
+    // questionNumber: 1,
+    question: "What is the correct way to get a button to invoke a function?",
     answers: [
-      { id: 1, answer: "1 first answer", type: "correct" },
-      { id: 2, answer: "1 second answer" },
-      { id: 3, answer: "1 third answer" },
-      { id: 4, answer: "1 fourth answer" },
+      { id: 1, answer: `In html file, add 'onClick = "function()"`, type: "correct" },
+      { id: 2, answer: "In js file, add 'onclick=function()" },
+      { id: 3, answer: "The only way to do it is in a function that calls to the button" },
+      { id: 4, answer: "In js file, use 'addEventListener' along with 2 or 3 arguments", type: "correct"},
     ],
   },
   {
     id: 1,
-    questionNumber: 2,
-    question: "This is the second question?",
+    // questionNumber: 2,
+    question: "CSS: How do you move text under one element on the left, but not under the element on the right?",
     answers: [
-      { id: 1, answer: "2 first answer" },
-      { id: 2, answer: "2 second answer" },
-      { id: 3, answer: "2 third answer" },
-      { id: 4, answer: "2 fourth answer" },
+      { id: 1, answer: "float: left;" },
+      { id: 2, answer: "float: right;" },
+      { id: 3, answer: "clear: left;" , type: "correct"},
+      { id: 4, answer: "float: right:" },
     ],
   },
   {
     id: 2,
-    questionNumber: 3,
-    question: "This is the third question?",
+    // questionNumber: 3,
+    question: "CSS: What is a 'normal' font weight?",
     answers: [
-      { id: 1, answer: "3 1st answer" },
-      { id: 2, answer: "3 2nd answer" },
-      { id: 3, answer: "3 3rd answer" },
-      { is: 4, answer: "3 4th answer" },
+      { id: 1, answer: "100" },
+      { id: 2, answer: "200" },
+      { id: 3, answer: "400", type: "correct" },
+      { id: 4, answer: "600" },
+    ],
+  },
+  {
+    id: 3,
+    // questionNumber: 3,
+    question: "How does html know how to use the javascript file?",
+    answers: [
+      { id: 1, answer: "It wil automatically use the index.js file." },
+      { id: 2, answer: "You need to tell it what to use in the package.json files." },
+      { id: 3, answer: "You need to tell it what to use in the head of the html file using the script tag.", type: "correct" },
+      { id: 4, answer: "You need to tell it what to usse in the javascript file." },
+    ],
+  },
+  {
+    id: 4,
+    // questionNumber: 3,
+    question: "CSS: What is a 'normal' font weight?",
+    answers: [
+      { id: 1, answer: "100" },
+      { id: 2, answer: "200" },
+      { id: 3, answer: "400", type: "correct" },
+      { id: 4, answer: "600" },
+    ],
+  },
+  {
+    id: 5,
+    // questionNumber: 3,
+    question: "CSS: What is a 'normal' font weight?",
+    answers: [
+      { id: 1, answer: "100" },
+      { id: 2, answer: "200" },
+      { id: 3, answer: "400", type: "correct" },
+      { id: 4, answer: "600" },
+    ],
+  },
+  {
+    id: 6,
+    // questionNumber: 3,
+    question: "CSS: What is a 'normal' font weight?",
+    answers: [
+      { id: 1, answer: "100" },
+      { id: 2, answer: "200" },
+      { id: 3, answer: "400", type: "correct" },
+      { id: 4, answer: "600" },
+    ],
+  },
+  {
+    id: 7,
+    // questionNumber: 3,
+    question: "CSS: What is a 'normal' font weight?",
+    answers: [
+      { id: 1, answer: "100" },
+      { id: 2, answer: "200" },
+      { id: 3, answer: "400", type: "correct" },
+      { id: 4, answer: "600" },
+    ],
+  },
+  {
+    id: 8,
+    // questionNumber: 3,
+    question: "CSS: What is a 'normal' font weight?",
+    answers: [
+      { id: 1, answer: "100" },
+      { id: 2, answer: "200" },
+      { id: 3, answer: "400", type: "correct" },
+      { id: 4, answer: "600" },
+    ],
+  },
+  {
+    id: 9,
+    // questionNumber: 3,
+    question: "CSS: What is a 'normal' font weight?",
+    answers: [
+      { id: 1, answer: "100" },
+      { id: 2, answer: "200" },
+      { id: 3, answer: "400", type: "correct" },
+      { id: 4, answer: "600" },
     ],
   },
 ];
@@ -46,19 +123,22 @@ let numCorrect = (startNumCorrect.innerHTML = 0);
 //start the game and populate the questions with the start button
 startButton.addEventListener("click", startGame);
 
-//Iterate over the q and a's to populate
-function questionIterator() {
+//this is not working
+function clickAnswer() {
+  let answerButton = document.getElementsByClassName("answer");
   for (let i = 0; i < qanda.length; i++) {
-    currentQuestionArray = qanda[i];
+    var qid = qanda[i].id;
   }
+  currentQuestionArray = qid;
+  console.log(`qid = ${qid}`);
+  answerButton.addEventListener("click", clickAnswer);
 }
-//the below code works to display the first question
-// let i = 0;
-// let currentQuestionArray = qanda[i];
 
 //populate q and a's
 function displayQandA() {
-  questionIterator();
+  // for (let i=0; i < qanda.length; i++){}
+  let i = 0; //this is a place holder. need to iterate through  qanda with an event listener
+  let currentQuestionArray = qanda[i];
   let currentQuestion = currentQuestionArray.question;
   let questionElement = document.createElement("h3");
   let questionText = currentQuestion;
